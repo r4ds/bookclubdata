a63f2820aa6813943de211ab8c8a7407982c08b7
Jon Harmon <jonthegeek@gmail.com>
2023-10-25 07:39:33 -0500

    Active clubs utc (#17)
    * Include datetime_utc in club data.
    
    * Bump vnum.

--- R/active-clubs.R
+++ R/active-clubs.R
@@ -5,19 +5,24 @@
 #' out of date. Once this function has been called in a session, it always
 #' returns the same result unless it is told to refresh.
 #'
-#' @param refresh Set this to `TRUE` to check for updates to the Google Sheet.
+#' @param effective_date Pass a specific date to update the cached value.
 #'
-#' @return A tibble with columns `club`, `day_utc`, `hour_utc`, `facilitator`,
-#'   and `book_abbr`.
+#' @return A tibble with columns `club`, `day_utc`, `hour_utc`, and `date_utc`.
 #' @export
-active_clubs_times <- function(refresh = FALSE) {
-  .cached_sheet_fetch(
-    refresh = refresh,
+active_clubs_times <- function(effective_date = lubridate::today()) {
+  active_clubs <- .cached_sheet_fetch(
+    refresh = TRUE,
     rds_file_id = "1qvi47OCP14zruj8cnOR3AvlWUR-TBICc",
     googlesheet_id = "1xZbG0IMgvV1ySTA1dOvdM6GuikrHPhePJxYDqzTratA",
     read_args = list(sheet = "raw_clubs"),
     cleaner_fn = .active_clubs_clean
   )
+  active_clubs$datetime_utc <- make_datetimes_utc(
+    days = active_clubs$day_utc,
+    hours = active_clubs$hour_utc,
+    timezones = "UTC"
+  )
+  return(active_clubs)
 }
 
 .active_clubs_clean <- function(active_clubs) {
--- R/zzz.R
+++ R/zzz.R
@@ -2,5 +2,6 @@
   .googledrive_authorize <<- memoise::memoise(.googledrive_authorize)
   .googlesheets_authorize <<- memoise::memoise(.googlesheets_authorize)
   .cached_sheet_impl <<- memoise::memoise(.cached_sheet_impl)
+  active_clubs_times <<- memoise::memoise(active_clubs_times)
   .rds_read_impl <<- memoise::memoise(.rds_read_impl)
 } # nocov end
